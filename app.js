770cb6e5d1a099977f1fb2d692e2755f3ad03e66
Sibangi Boxipatro <pragya.baxipatra@gmail.com>
2025-06-21 21:55:45 +0530

    This js file is linked to the modules.html file

---
+++ app.js
@@ -0,0 +1,16 @@
+import {add, subtract, pi} from "./src/mathUtils.js";
+
+import message from "./src/message.js";
+
+console.log(add(3,2));
+console.log(subtract(3,2));
+console.log(pi);
+
+message("Sibangi");
+
+import { counter,incrementCounter }  from "./src/counter.js";
+
+document.getElementById("updateCounter").addEventListener("click", ()=>{
+  incrementCounter();
+  document.getElementById("counter").textContent=counter;
+});
